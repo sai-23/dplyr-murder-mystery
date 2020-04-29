9f878ebb7cc16f2705e6a30ef720f8e03037bfdf
sai-23 <saishreyas.bhavanasi@slu.edu>
2020-04-29 12:09:19 -0500

    Added murder confession

--- R/SQL_city_to_dplyr_city.R
+++ R/SQL_city_to_dplyr_city.R
@@ -17,6 +17,11 @@ load("data/dplyr_murder_mystery.Rda")
 crime_scene_report$city <- ifelse(str_detect(crime_scene_report$city, "SQL"), 
                                   "dplyr City", 
                                   crime_scene_report$city)
+interview$transcript <- ifelse(str_detect(interview$transcript, "SQL"), 
+                               "I was hired by a woman with a lot of money. I don't know her name but I know she's around 65 or 67. She has red hair and she drives a Tesla Model S. I know that she attended the dplyr City Concert 3 times in December 2017.\n", 
+                               interview$transcript)
+
+interview[nrow(interview) + 1,] = list(99716,"I confess, I had no choice but to silence the man...")
 
 #'
 #' Save the data back into an Rda format
